6c8ee852be495eaee39e10fdf96d49416e8e22cd
anthonyargueta <157061197+anthonyargueta@users.noreply.github.com>
2024-10-21 01:29:53 -0500

    HW 9_2

---
+++ Argueta_HW9_2.cpp
@@ -0,0 +1,38 @@
+/*Filename: Line Numbers
+Description: Write a program that asks the user for the name of a file. The program should display the contents of the file on the screen. 
+Author: Anthony Argueta Moncada
+Date Modified: 10/18/24 */ 
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    ifstream inFile;
+    char inName[200];
+    string inputLine = " ";
+    int lineNumber = 1;
+
+    cout << "Enter the input file name: "; 
+    cin >> inName;
+    inFile.open(inName);
+    if (!inFile)
+    {
+        cout << " Error opening file " << inName << endl;
+        return 21;
+    }
+
+    while (!inFile.eof())
+    {
+        getline(inFile, inputLine, '\n');
+        cout << setw(4) << right << lineNumber 
+        << ": " << inputLine << endl;
+        lineNumber ++;   
+    }
+
+    inFile.close();
+
+    return 0;
+}
